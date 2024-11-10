b4947cdd0a80a4b3f804473b8c42512f840013c9
OhYuha <162884244+OhYuha@users.noreply.github.com>
2024-11-10 10:23:34 +0900

    오류를 수정했습니다.

--- main.ts
+++ main.ts
@@ -1,13 +1,8 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
     servos.P1.setAngle(100)
-    basic.pause(500)
+    basic.pause(1000)
     servos.P1.setAngle(-100)
+    basic.showIcon(IconNames.Heart)
 })
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showIcon(IconNames.Happy)
-        basic.pause(500)
-    } else {
-        basic.showIcon(IconNames.Heart)
-    }
-})
+basic.showIcon(IconNames.Heart)
