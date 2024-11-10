7f1218f98aeb7c459b2d5e11e5e967935c769ea1
OhYuha <162884244+OhYuha@users.noreply.github.com>
2024-11-10 19:30:19 +0900

    LED를 추가했습니다.

--- main.ts
+++ main.ts
@@ -1,10 +1,34 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
     servos.P1.setAngle(-300)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . . . .
+        . # . # .
+        . . . . .
+        `)
     basic.pause(500)
     servos.P1.setAngle(90)
     basic.pause(200)
-    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        # # # # #
+        . # . # .
+        `)
 })
-basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.AB, function () {
+    pins.analogWritePin(AnalogPin.P2, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.analogWritePin(AnalogPin.P2, 1023)
+})
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    # # # # #
+    . # . # .
+    `)
 servos.P1.setAngle(90)
